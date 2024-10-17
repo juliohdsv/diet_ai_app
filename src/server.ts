3506275664dd8d06f1e6ac45638fedcddaa392b8
juliohdsv <ricksv92@gmail.com>
2024-10-16 21:53:27 -0300

    feat(routes): created routes and configurated

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import fastify from "fastify";
-import cors from "cors";
+import cors from "@fastify/cors";
 import dotenv from "dotenv";
+import { routes } from "./routes/main.routes";
 
 const app = fastify({ logger: true});
 dotenv.config();
@@ -8,3 +9,17 @@ dotenv.config();
 app.setErrorHandler((error, request, reply) => {
   reply.code(400).send({ message: error.message })
 });
+
+const start = async ()=> {
+  app.register(cors);
+  app.register(routes);
+
+  try {
+    await app.listen({port: 3333, host:"0.0.0.0"})
+    console.log("Server running on port 3333");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+start();
